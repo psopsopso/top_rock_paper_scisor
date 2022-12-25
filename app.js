4793a7e7a0303a5ba3b013b4342eb1aeb33b2f87
Patrice Sorrant <48038618+psopsopso@users.noreply.github.com>
2022-12-24 20:49:17 -0500

    Add more game logic and UI

--- app.js
+++ app.js
@@ -1,49 +1,124 @@
-const CHOICES = ["Rock", "Paper", "Scissor"]
-let user_score = 0
-let computer_score = 0
+const CHOICES = ["Sword", "Fireball", "Crossbow"]
+const playBtn = document.querySelector("#play-btn")
+const weaponsContainer = document.querySelector(".weapons-container")
+const displayText = document.querySelector("#display-text")
+const swordBtn = document.querySelector("#sword-button")
+const crossbowBtn = document.querySelector("#crossbow-button")
+const fireballBtn = document.querySelector("#fireball-button")
+const playersChoiceWeapon = document.querySelector("#players-weapon")
+const computersChoiceWeapon = document.querySelector("#computers-weapon")
+
+let user_score = 3
+let computer_score = 3
+
+playBtn.addEventListener('click', () => { // Game Begin
+    playBtn.style.display = 'none';
+    displayText.innerHTML = 'FIGHT !';
+    weaponsContainer.style.display = 'block';
+});
+
+// Player's buttons event listeners. 
+
+swordBtn.addEventListener('click', () => {
+    updatePlayerDisplay("sword");
+    playRound("Sword");
+});
+
+crossbowBtn.addEventListener('click', () => {
+    updatePlayerDisplay("crossbow");
+    playRound("Crossbow");
+});
+
+fireballBtn.addEventListener('click', () => {    
+    updatePlayerDisplay("fireball");
+    playRound("Fireball");
+});
+
+// Helper functions
+
+function updatePlayerDisplay(choice) {
+    displayText.innerHTML = `${choice.toUpperCase()}!`;
+    playersChoiceWeapon.src = `sprites/${choice.toLowerCase()}.png`;
+}
+
+function updateComputerDisplay(computerChoice) {
+    displayText.innerHTML = `${computerChoice.toUpperCase()}!`;
+    computersChoiceWeapon.src = `sprites/${computerChoice.toLowerCase()}.png`;
+}
 
 function getComputerChoice() {
     return CHOICES[Math.floor(Math.random()*CHOICES.length)]; 
 }
 
-function playRound(playerSelection, computerSelection) {
-    let playerSelectionLowerCase = playerSelection.toLowerCase();
-    let computerSelectionLowerCase = computerSelection.toLowerCase();
-    if (playerSelectionLowerCase === computerSelectionLowerCase) {
-        return `Player's : ${playerSelection.toUpperCase()}. Computer's : ${computerSelection.toUpperCase()}. Draw!`;
+// Updating player or computer heart count
+
+function playerWinsRound() {
+    hideHeart("computer");
+    displayText.innerHTML = "TAKE THAT !";
+  }
+  
+function computerWinsRound() {
+    displayText.innerHTML = "OUCH !"
+    hideHeart("player");
+}
+
+function hideHeart(player) {
+    var heartContainers = document.querySelector(`.heart-containers-${player}`);
+    var hearts = Array.from(heartContainers.querySelectorAll(".heart-img"));
+    var visibleHearts = hearts.filter(heart => heart.style.display !== "none");
+    if (visibleHearts.length > 0) {
+      visibleHearts[visibleHearts.length - 1].style.display = "none";
     }
-    else if (playerSelectionLowerCase === "rock" && computerSelectionLowerCase === "scissor") {
-        user_score += 1;
-        return `Player's : ${playerSelection.toUpperCase()}. Computer's : ${computerSelection.toUpperCase()}. You win!`
+  }
+
+// Compute player scores based on their choices
+
+function findRoundWinner(playerSelection,computerChoice) {
+    if (playerSelection === computerChoice) {
+        displayText.innerHTML = "DRAW !"
     }
-    else if (playerSelectionLowerCase === "paper" && computerSelectionLowerCase === "rock") {
-        user_score += 1;
-        return `Player's : ${playerSelection.toUpperCase()}. Computer's : ${computerSelection.toUpperCase()}. You win!`;
+    else if (playerSelection === "Sword" && computerChoice === "Fireball") {
+        user_score -= 1;
+        computerWinsRound();
     }
-    else if (playerSelectionLowerCase === "scissor" && computerSelectionLowerCase === "paper") {
-        user_score += 1;
-        return `Player's : ${playerSelection.toUpperCase()}. Computer's : ${computerSelection.toUpperCase()}. You win!`;
+    else if (playerSelection === "Fireball" && computerChoice === "Crossbow") {
+        user_score -= 1;
+        computerWinsRound();
+    }
+    else if (playerSelection === "Crossbow" && computerChoice === "Sword") {
+        user_score -= 1;
+        computerWinsRound();
     }
     else {
-        computer_score += 1;
-        return `Player's : ${playerSelection.toUpperCase()}. Computer's : ${computerSelection.toUpperCase()}. You lose!`;
+        computer_score -= 1;
+        playerWinsRound();
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let user_choice = prompt("ROCK, PAPER, SCISSOR! What do you pick ?");
-        console.log(playRound(user_choice, getComputerChoice()));
-        console.log(`Score : player: ${user_score}. Computer: ${computer_score}.`);
+function findGameWinner() {
+    if (user_score === 0) {
+        gameEnd("loss");
     }
-    if (user_score > computer_score) {
-        console.log("You win!");
-    }
-    else if (user_score < computer_score) {
-        console.log("You lose!");
-    }
-    else {
-        console.log("It's a draw!");
+    else if (computer_score === 0) {
+        gameEnd("win");
     }
 }
 
+function gameEnd(outcome) {
+    if (outcome === "loss") {
+        displayText.innerHTML = "YOU LOSE ;(";
+    }
+    else {
+        displayText.innerHTML = "YOU WIN ^^ !";
+    }
+    setTimeout(() => location.reload(), 3000);
+}
+
+function playRound(playerSelection) {
+    let computerChoice = getComputerChoice();
+    setTimeout(updateComputerDisplay, 1000, computerChoice);
+    setTimeout(findRoundWinner, 2000, playerSelection, computerChoice);
+    setTimeout(findGameWinner, 3000);
+    console.log(user_score);
+    console.log(computer_score);
+}
